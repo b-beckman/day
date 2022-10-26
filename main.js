e7f3cd0fdb6235b01c600d5bdcabae7fe1cd95f6
b-beckman <leobuess.pro@gmail.com>
2022-10-26 09:48:29 +0200

    Added average function

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
 console.log("ça fonctionne!");
+
+let ta = [4, 4, 5];
+let total = 0;
+
+function average(tableau) {
+  for (const note of tableau) {
+    total = total + note;
+  }
+  let moyenne = total / tableau.length;
+  return total / tableau.length;
+}
+
+console.log(average(ta));
+average(ta);
