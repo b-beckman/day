ac33f97decb4cf6d57b3501196bb4b1aad0e468f
b-beckman <leobuess.pro@gmail.com>
2022-10-26 14:54:43 +0200

    fixed average function

--- main.js
+++ main.js
@@ -13,3 +13,48 @@ toto.addEventListener("input", function (e) {
     document.getElementsByTagName("header")[0].innerText = e.target.value;
   }
 });
+
+
+
+
+
+//Calculate my grades average
+
+//Display Average
+function display(avg, id){
+  document.getElementById(id).innerText = avg
+}
+//Calculate Average
+function average (gradesArray){
+  console.log(gradesArray)
+  let sum = 0
+  for (const grades of gradesArray) {
+    sum +=grades
+  }
+  console.log(sum/gradesArray.length)
+  return sum/gradesArray.length
+}
+//retrieves Grades
+
+function getGrades(gradesID) {
+  let grades = []
+  for (const gradesIDElement of gradesID) {
+    let grade = parseFloat(document.getElementById(gradesIDElement).value)
+    if(!isNaN(grade)){
+      grades.push(grade)
+    }
+
+  }
+  return grades
+}
+
+
+let inputs = document.getElementsByTagName("input")
+for (const input of inputs) {
+  input.addEventListener("input",function (){
+    display(average(getGrades(["note 1", "note 2"])),"avg")
+  })
+
+}
+
+
