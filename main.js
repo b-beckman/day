792586398d6fa3720a65d19c09d00e6a8931d7fd
b-beckman <leobuess.pro@gmail.com>
2022-10-26 11:28:24 +0200

    Added changes to main.js

--- main.js
+++ main.js
@@ -7,6 +7,13 @@ let toto = document.getElementById("les-differents-types-de-grimpe");
 toto.addEventListener("click", function () {
   let articles = document.getElementsByTagName("article");
   for (const article of articles) {
-    article.style.backgroundColor = "blue";
+    article.style.backgroundColor =
+      "#" + Math.floor(Math.random() * 16777215).toString(16);
+  }
+});
+toto.addEventListener("click", function () {
+  const divElem = document.getElementById("les-differents-types-de-grimpe");
+  function randomcolor() {
+    return Math.floor(Math.random() * 255);
   }
 });
