4dd1d22fbdebaff948c0bf8489a4c838b750387b
b-beckman <leobuess.pro@gmail.com>
2022-10-26 13:00:35 +0200

    up to date

--- main.js
+++ main.js
@@ -1,13 +1,15 @@
 console.log("Bonjour navigateur!");
 
-let toto = document.getElementById("les-differents-types-de-grimpe");
+let toto = document.getElementById("textarea");
 
 //toto.innerText = "salut les gars";
 
-toto.addEventListener("click", function () {
+toto.addEventListener("input", function (e) {
   let articles = document.getElementsByTagName("article");
   for (const article of articles) {
     article.style.backgroundColor =
       "#" + Math.floor(Math.random() * 16777215).toString(16);
+
+    document.getElementsByTagName("header")[0].innerText = e.target.value;
   }
 });
